1b663406a39eb94cc29662a67d9d4083d5f3696d
Lyle Irvine <63163556+Caesar-Sec@users.noreply.github.com>
2020-09-20 08:33:42 +0100

    Update takeover_stripped.sh

--- takeover_stripped.sh
+++ takeover_stripped.sh
@@ -4,7 +4,8 @@ cd ~/tools/tmp
 ulimit -n 7000;
 
 #Check for dangling CNAME Records
-~/go/bin/subjack -w ~/tools/tmp/subs.txt -t 1000 -timeout 15 -v -c ~/go/src/github.com/haccer/subjack$cat results.txt | grep -v "Not Vulnerable" > takeover.txt;
+~/go/bin/subjack -w ~/tools/tmp/subs.txt -t 1000 -timeout 15 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -a -ssl -o results.txt;
+cat results.txt | grep -v "Not Vulnerable" > takeover.txt;
 
 
 #Send data to discord webhook
@@ -26,16 +27,26 @@ cat takeover.txt | grep -i '[Heroku]' > tmp.txt;
 cat takeover.txt | grep -i '[DOMAIN AVAILABLE' > tmp.txt;
 ~/go/bin/discordmessage "$(< ./tmp.txt)";
 
+cat takeover.txt | grep -i '[Digital Ocean]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Wordpress]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
 
 #Diff new and last takeover.txt
-comm -13 takeover_old.txt takeover.txt > tmp.txt;
+cat takeover_old.txt | sort | tee -a diff1.txt
+cat takeover.txt | sort | tee -a diff2.txt
+comm -13 diff1.txt diff2.txt > tmp.txt;
 ~/go/bin/discordtitle Diff;
 ~/go/bin/discordmessage "$(< ./tmp.txt)";
 mv takeover.txt takeover_old.txt;
-
+rm diff1.txt
+rm diff2.txt
 
 #Clean up
 cp takeover.txt ../backups/"takeover-`date +"%d-%m-%Y-%H:%M"`.txt";
 echo "Done" - `date +"%d-%m-%Y-%H:%M"` >> finished.txt;
 rm results.txt;
 rm tmp.txt;
+
