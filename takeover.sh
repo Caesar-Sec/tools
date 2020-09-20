e0df6e241390942639cf7c8c5b8f6385ee201348
Lyle Irvine <63163556+Caesar-Sec@users.noreply.github.com>
2020-09-20 08:11:59 +0100

    Update takeover.sh

--- takeover.sh
+++ takeover.sh
@@ -1,53 +1,135 @@
 #!/bin/bash
 
-#Get wildcarded bounty data
-cd ~/takeover;
-git clone https://github.com/arkadiyt/bounty-targets-data;
-cp ~/takeover/bounty-targets-data/data/wildcards.txt ./;
-cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > root_domains.txt;
-rm -rf bounty-targets-data/;
-rm -f wildcards.txt;
+cd ~/tools/tmp
 ulimit -n 7000;
 
-
-#Find Subdomains
-while read host;
-    do file=subdomains.txt;
-    ~/go/bin/subfinder -o $file -d $host;
-    ~/go/bin/assetfinder --subs-only $host | tee -a $file;
-    cat $file | sort -u | tee -a subs.txt;
-done < ./root_domains.txt;
-
-
 #Check for dangling CNAME Records
-~/go/bin/subjack -w subs.txt -t 250 -timeout 20 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -a -ssl -o results.txt;
+~/go/bin/subjack -w ~/tools/tmp/subs.txt -t 1000 -timeout 15 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -a -ssl -o results.txt;
 cat results.txt | grep -v "Not Vulnerable" > takeover.txt;
 
 
 #Send data to discord webhook
-cat takeover.txt | grep -i 'Fastly' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[Fastly]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i 'Azure' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[Azure]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i 'Github' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[Github]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i 'S3 BUCKET' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[S3 BUCKET]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i 'Heroku' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[Heroku]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i 'DOMAIN AVAILABLE' > tmp.txt
-go run ~/go/src/github.com/caesarsec/discordmessage/discordmessage.go '```'"$(< ./tmp.txt)"'```'
+cat takeover.txt | grep -i '[DOMAIN AVAILABLE' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Agile CRM]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Anima]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Worksites]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Bitbucket]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Campaign Monitor]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Cargo Collective]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Cargo Collective]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Digital Ocean]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Feedpress]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Fly.io]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Gemfury]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Ghost]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[HatenaBlog]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Help Juice]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Help Scout]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Intercom]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[JetBrains]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Kinsta]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[LaunchRock]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Ngrok]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Pantheon]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Pingdom]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Readme.io]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[SmartJobBoard]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Statuspage]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Strikingly]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Surge.sh]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Uberflip]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Uptimerobot]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[UserVoice]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+cat takeover.txt | grep -i '[Wordpress]' > tmp.txt;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+
+
+#Diff new and last takeover.txt
+comm -13 takeover_old.txt takeover.txt > tmp.txt;
+~/go/bin/discordtitle Diff;
+~/go/bin/discordmessage "$(< ./tmp.txt)";
+mv takeover.txt takeover_old.txt;
 
 
 #Clean up
-cp takeover.txt ../backups/"takeover-`date +"%d-%m-%Y"`.txt"
-echo "Done" - `date +"%d-%m-%Y-%H-%M"` >> finished.txt
-rm root_domains.txt;
+cp takeover.txt ../backups/"takeover-`date +"%d-%m-%Y-%H:%M"`.txt";
+echo "Done" - `date +"%d-%m-%Y-%H:%M"` >> finished.txt;
 rm results.txt;
-rm subs.txt;
-rm subdomains.txt;
+rm tmp.txt;
