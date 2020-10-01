710004b22206b0a9606a258c8708394c6b9f154c
Lyle Irvine <63163556+Caesar-Sec@users.noreply.github.com>
2020-10-01 13:38:17 +0100

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -2,24 +2,18 @@
 
 #Get wildcarded bounty data
 cd ~/tools/tmp;
-git clone https://github.com/arkadiyt/bounty-targets-data;
-cp ./bounty-targets-data/data/wildcards.txt ./;
-cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > root_domains.txt;
-rm -rf bounty-targets-data/;
-rm -f wildcards.txt;
 ulimit -n 7000;
-
 
 #Find Subdomains
 while read host;
     do file=subdomains.txt;
-    ~/go/bin/subfinder -o $file -d $host;
+    ~/go/bin/subfinder -config ~/.config/subfinder/config.yaml -o $file -d $host;
+    ~/go/bin/amass enum -passive -d $host -o $file;
     ~/go/bin/assetfinder --subs-only $host | tee -a $file;
     cat $file | sort -u | tee -a subs1.txt;
-done < ./root_domains.txt;
+done < ./domains.txt;
 
 #Cleanup
 cp subs1.txt subs.txt;
 rm subdomains.txt;
 rm subs1.txt;
-rm root_domains.txt;
