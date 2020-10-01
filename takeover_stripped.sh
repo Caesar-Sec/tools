4ffe6f7bfdfa44e1715592e0ec301b95ddd0f101
Lyle Irvine <63163556+Caesar-Sec@users.noreply.github.com>
2020-10-01 13:37:49 +0100

    Update takeover_stripped.sh

--- takeover_stripped.sh
+++ takeover_stripped.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
-cd ~/tools/tmp
-ulimit -n 7000;
+cd ~/tools/tmp;
+ulimit -n 5000;
+FILENAME=$(date +"%d-%m-%Y-%H:%M");
+OUTPUT=$(echo "results-"$FILENAME".txt");
+
 
 #Check for dangling CNAME Records
-~/go/bin/subjack -w ~/tools/tmp/subs.txt -t 1000 -timeout 15 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -a -ssl -o results.txt;
-cat results.txt | grep -v "Not Vulnerable" > takeover.txt;
+~/go/bin/subjack -w ~/tools/tmp/subs.txt -t 250 -timeout 10 -v -c ~/go/src/github.com/haccer/subjack/fingerprints.json -a -ssl -o results.txt;
+cat ~/tools/tmp/"$OUTPUT" | grep -v "Not Vulnerable" >> ~/tools/tmp/takeover.txt;
 
 
 #Send data to discord webhook
-cat takeover.txt | grep -i '[Fastly]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
 
-cat takeover.txt | grep -i '[Azure]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+~/go/bin/discorder "Finished `date +"%d-%m-%Y-%H:%M"`";
 
-cat takeover.txt | grep -i '[Github]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'Fastly' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-cat takeover.txt | grep -i '[S3 BUCKET]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'Azure' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-cat takeover.txt | grep -i '[Heroku]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'Github' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-cat takeover.txt | grep -i '[DOMAIN AVAILABLE' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'S3 BUCKET' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-cat takeover.txt | grep -i '[Digital Ocean]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'DOMAIN AVAILABLE' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-cat takeover.txt | grep -i '[Wordpress]' > tmp.txt;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
+cat ~/tools/tmp/takeover.txt | grep -i 'Digital Ocean' > ~/tools/tmp/tmp.txt;
+[ -s ~/tools/tmp/tmp.txt ] && ~/go/bin/discorder "$(< ~/tools/tmp/tmp.txt)" || echo "File empty";
 
-
-#Diff new and last takeover.txt
-cat takeover_old.txt | sort | tee -a diff1.txt
-cat takeover.txt | sort | tee -a diff2.txt
-comm -13 diff1.txt diff2.txt > tmp.txt;
-~/go/bin/discordtitle Diff;
-~/go/bin/discordmessage "$(< ./tmp.txt)";
-mv takeover.txt takeover_old.txt;
-rm diff1.txt
-rm diff2.txt
 
 #Clean up
-cp takeover.txt ../backups/"takeover-`date +"%d-%m-%Y-%H:%M"`.txt";
+cp ~/tools/tmp/takeover.txt ~/backups/"takeover-`date +"%d-%m-%Y-%H:%M"`.txt";
 echo "Done" - `date +"%d-%m-%Y-%H:%M"` >> finished.txt;
-rm results.txt;
+mv takeover.txt takeover_old.txt;
+rm "$OUTPUT";
 rm tmp.txt;
 
