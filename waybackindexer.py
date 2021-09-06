c4aada5f771229ff2f5ab5c76410211d887c96cc
Lyle-Irvine <caesar_sec@protonmail.com>
2021-09-06 19:13:54 +0100

    script works, output needs split to separate files

--- waybackindexer.py
+++ waybackindexer.py
@@ -22,7 +22,6 @@ def indexes(host):
 
 
 def get_data(snapshot):
-    print("Staring New Page")
     url = 'https://web.archive.org/web/{0}/{1}'.format(snapshot[0], snapshot[1]) 
     indexPage = requests.get(url).text
     soup = bs(indexPage, "html.parser")
@@ -31,11 +30,7 @@ def get_data(snapshot):
     input_tags = soup.find_all('input')
     for i in input_tags:
         try:
-            input_attributes.add(i['id'])
-        except:
-            pass
-        try:
-            input_attributes.add(i['name'])
+            append_input(INPUT_ATTRIBUTES, i)
         except:
             pass
 
@@ -44,19 +39,32 @@ def get_data(snapshot):
     for i in js_paths:
         # add an if to check if the url is relative or absolute and append the host
         try:
-            js_attributes.add(i['src'])
+            append_js(JS_ATTRIBUTES, i)
         except:
             pass
-    return input_attributes, js_attributes
+    return
         
+
+def append_input(input_list, value):
+    input_list.append(value['id'])
+    input_list.append(value['name'])
+    return input_list
+
+
+def append_js(js_list, value):
+    js_list.append(value['src'])
+    return js_list
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: \npython3 waybackindexer.py <domain-name>')
         sys.exit()
-    
+
     host = sys.argv[1]
+    if "http" not in host:
+        host = 'https://' + host
+
 
     snapshots = indexes(host)
     if len(snapshots) == 0:
@@ -66,23 +74,34 @@ if __name__ == '__main__':
     
     # get all input unique attributes
     print("Staring Execution")
-    input_attributes = list()
-    js_attributes = list()
-    input_attributes, js_attributes = pool.map(get_data, snapshots[1:5])
+    INPUT_ATTRIBUTES = list()
+    JS_ATTRIBUTES = list()
+    pool.map(get_data, snapshots[1:20])
+
+    # move data into a set from the list
     uniqueAttributes = set()
-    for i in input_attributes:
-        uniqueAttributes.update(i)
+    uniqueJS = set()
 
-    # Write to output files
-    print(type(uniqueAttributes))
-    print(type(js_attributes))
-    print(uniqueAttributes)
-    print(js_attributes)
-    # f_hand = open('waybackindexer.txt', 'w')
-    # for i in uniqueAttributes:
-    #     f_hand.write(i + '\n')
-    # f_hand.write('\n\n\n')
-    # for c in js_attributes:
-    #     f_hand.write(c + '\n')
-    # f_hand.close()
+    for i in INPUT_ATTRIBUTES:
+        uniqueAttributes.add(i)
 
+    for c in JS_ATTRIBUTES:
+        if "archive.org" in c:
+            pass
+        elif c[0:4] != "http" or c[0:4] != "HTTP":
+            if c[0:5] == "/web/":
+                x = c.find("http")
+                result = c[x:]
+                uniqueJS.add(result)
+            elif "http" not in c:
+                new_c = host + c
+                uniqueJS.add(new_c)
+        else:
+            uniqueJS.add(c)
+
+
+    filename = "waybackindexer.txt"
+    with open(filename, 'w') as f_hand:
+        f_hand.write('\n'.join(uniqueAttributes))
+        f_hand.write('\n\n')
+        f_hand.write('\n'.join(uniqueJS))
